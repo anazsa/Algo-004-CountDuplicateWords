b2a481b8ba99f58c479af89441a93e71a6cebfbe
Anazsa Blanding <anazsabee@Anazsas-MacBook-Air.local>
2019-08-05 09:48:57 -0400

    Notdone

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -2,7 +2,34 @@ package hbcu.stay.ready.algor;
 
 public class Solution {
 
-    public String countUniqueWords(String input){
-        return null;
+    public String countUniqueWords(String input) {
+        String words = "";
+
+        int count = 0 ;
+
+        for ( count = 0; count < input.length(); count++);
+
+        String [] x = input.split("");
+
+        String [] y = input.split(".");
+
+        {
+            
+
+        }
+
+
+
+        {
+
+            System.out.print("The 3 unique words are"+ "\n");
+            System.out.println("Hello Seen" + "");
+
+
+
+        }
+
+
+                return input;
     }
 }
